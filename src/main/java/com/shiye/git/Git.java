228ef215581b36da9bb17905053b74531ddd2e71
shi-ye-ye <1467851319@qq.com>
2022-08-03 10:41:45 +0800

    first commit

---
+++ src/main/java/com/shiye/git/Git.java
@@ -0,0 +1,12 @@
+package com.shiye.git;
+
+/**
+ * @Author: 石烨
+ * @Date: 2022/08/03/10:36
+ * @Description:
+ */
+public class Git {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
